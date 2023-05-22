c7384d417096d9134890e18632529dd7066d7dc8
Eric Arellano <ericarellano@me.com>
2023-05-22 17:53:07 -0600

    Add Pants so I can more easily install deps

--- enrich_data.py
+++ enrich_data.py
@@ -2,13 +2,16 @@ import csv
 from pathlib import Path
 from typing import Any
 
+
 def main() -> None:
     with Path("prn_contact_addresses.csv").open() as f:
         original_data = csv.DictReader(f)
         result = list(original_data)
     result = [process_row(row) for row in result]
     with Path("result.csv").open("w", newline="") as f:
-        writer = csv.DictWriter(f, fieldnames=original_data.fieldnames, quoting=csv.QUOTE_ALL)
+        writer = csv.DictWriter(
+            f, fieldnames=original_data.fieldnames, quoting=csv.QUOTE_ALL
+        )
         writer.writeheader()
         writer.writerows(result)
 
@@ -28,6 +31,5 @@ def process_row(row: dict[str, Any]) -> dict[str, Any]:
     return result
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
