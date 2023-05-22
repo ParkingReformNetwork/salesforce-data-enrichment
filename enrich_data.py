633b32b97b7c5f44c9f4cd4606ed3788583cf4eb
Eric Arellano <ericarellano@me.com>
2023-05-22 17:23:19 -0600

    Normalize US country to USA

--- enrich_data.py
+++ enrich_data.py
@@ -6,14 +6,19 @@ def main() -> None:
     with Path("prn_contact_addresses.csv").open() as f:
         original_data = csv.DictReader(f)
         result = list(original_data)
-    result = normalize_country_code(result)
+    result = [process_row(row) for row in result]
     with Path("result.csv").open("w", newline="") as f:
         writer = csv.DictWriter(f, fieldnames=original_data.fieldnames, quoting=csv.QUOTE_ALL)
         writer.writeheader()
         writer.writerows(result)
 
-def normalize_country_code(data: list[dict[str, Any]]) -> list[dict[str, Any]]:
-    return data
+
+def process_row(row: dict[str, Any]) -> dict[str, Any]:
+    result = {**row}
+    if result["MailingCountry"] == "US":
+        result["MailingCountry"] = "USA"
+    return result
+
 
 
 if __name__ == '__main__':
