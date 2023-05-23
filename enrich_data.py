a449cd8bd7c4ee9fc2b3b46f6f36007d90533084
Eric Arellano <ericarellano@me.com>
2023-05-23 14:48:06 -0600

    Fill in 2 addresses from Mailchimp hardcoded address

--- enrich_data.py
+++ enrich_data.py
@@ -4,12 +4,20 @@ from typing import Any
 
 from uszipcode import SearchEngine
 
+from hardcoded_addresses import ADDRESS_STRINGS
+
 
 def main() -> None:
     with Path("prn_contact_addresses.csv").open() as f:
         original_data = csv.DictReader(f)
         result = list(original_data)
-    result = [process_row(row) for row in result]
+    with Path("subscribed_members_export.csv").open() as f:
+        raw_subscribed_members_data = csv.DictReader(f)
+        subscribed_members_by_email = {
+            row["Email Address"]: {**row} for row in raw_subscribed_members_data
+        }
+
+    result = [process_row(row, subscribed_members_by_email) for row in result]
     with Path("result.csv").open("w", newline="") as f:
         writer = csv.DictWriter(
             f, fieldnames=original_data.fieldnames, quoting=csv.QUOTE_ALL
@@ -18,8 +26,26 @@ def main() -> None:
         writer.writerows(result)
 
 
-def process_row(row: dict[str, Any]) -> dict[str, Any]:
+def process_row(
+    row: dict[str, Any], subscribed_members_by_email: dict[str, dict[str, Any]]
+) -> dict[str, Any]:
     result = {**row}
+
+    # Enrich Salesforce data with Mailchimp data.
+    if (email := row["Email"]) in subscribed_members_by_email:
+        mailchimp_data = subscribed_members_by_email[email]
+        if mailchimp_data["Address"]:
+            mailchimp_addr = ADDRESS_STRINGS[mailchimp_data["Address"]]
+            if not result["MailingCountry"]:
+                result["MailingCountry"] = mailchimp_addr.country
+            if not result["MailingState"]:
+                result["MailingState"] = mailchimp_addr.state
+            if not result["MailingCity"]:
+                result["MailingCity"] = mailchimp_addr.city
+            if not result["MailingPostalCode"]:
+                result["MailingPostalCode"] = mailchimp_addr.postal
+            if not result["MailingStreet"]:
+                result["MailingStreet"] = mailchimp_addr.street
 
     # Normalize country code.
     if result["MailingCountry"] in ("US", "United States"):
