2859a67185e152cd2faa1d77b5a68722f9866ca8
Eric Arellano <ericarellano@me.com>
2023-05-23 16:32:54 -0600

    Find locatio of 480 records via reverse geocoding and city/state field

--- enrich_data.py
+++ enrich_data.py
@@ -2,6 +2,7 @@ import csv
 from pathlib import Path
 from typing import Any
 
+from geopy import Nominatim
 from uszipcode import SearchEngine
 
 from hardcoded_addresses import ADDRESS_STRINGS
@@ -9,33 +10,48 @@ from hardcoded_addresses import ADDRESS_STRINGS
 
 def main() -> None:
     with Path("prn_contact_addresses.csv").open() as f:
-        original_data = csv.DictReader(f)
-        result = list(original_data)
+        raw_original_data = csv.DictReader(f)
+        original_data = list(raw_original_data)
     with Path("subscribed_members_export.csv").open() as f:
         raw_subscribed_members_data = csv.DictReader(f)
         subscribed_members_by_email = {
             row["Email Address"]: {**row} for row in raw_subscribed_members_data
         }
 
-    result = [process_row(row, subscribed_members_by_email) for row in result]
+    zipcode_search_engine = SearchEngine()
+    geocoder = Nominatim(user_agent="parking_reform_network_data_enrichment")
+    result = [
+        process_row(row, zipcode_search_engine, geocoder, subscribed_members_by_email)
+        for row in original_data
+    ]
     with Path("result.csv").open("w", newline="") as f:
         writer = csv.DictWriter(
-            f, fieldnames=original_data.fieldnames, quoting=csv.QUOTE_ALL
+            f, fieldnames=raw_original_data.fieldnames, quoting=csv.QUOTE_ALL
         )
         writer.writeheader()
         writer.writerows(result)
 
 
 def process_row(
-    row: dict[str, Any], subscribed_members_by_email: dict[str, dict[str, Any]]
+    row: dict[str, Any],
+    zipcode_search_engine: SearchEngine,
+    geocoder: Nominatim,
+    subscribed_members_by_email: dict[str, dict[str, Any]],
 ) -> dict[str, Any]:
     result = {**row}
 
-    # Enrich Salesforce data with Mailchimp data.
-    if (email := row["Email"]) in subscribed_members_by_email:
+    # Enrich Salesforce location data with Mailchimp data.
+    if (email := row["Email"]) in subscribed_members_by_email and (
+        not result["MailingCountry"]
+        or not result["MailingState"]
+        or not result["MailingCity"]
+        or not result["MailingPostalCode"]
+    ):
         mailchimp_data = subscribed_members_by_email[email]
-        if mailchimp_data["Address"]:
-            mailchimp_addr = ADDRESS_STRINGS[mailchimp_data["Address"]]
+
+        # If there's an exact address, use that.
+        if addr := mailchimp_data["Address"]:
+            mailchimp_addr = ADDRESS_STRINGS[addr]
             if not result["MailingCountry"]:
                 result["MailingCountry"] = mailchimp_addr.country
             if not result["MailingState"]:
@@ -46,6 +62,34 @@ def process_row(
                 result["MailingPostalCode"] = mailchimp_addr.postal
             if not result["MailingStreet"]:
                 result["MailingStreet"] = mailchimp_addr.street
+
+        # Next, reverse geocode the lat/long. This allows us to get a zip code.
+        elif (lat := mailchimp_data["LATITUDE"]) and (
+            long := mailchimp_data["LONGITUDE"]
+        ):
+            long = long.lstrip("'")
+            addr = geocoder.reverse(f"{lat}, {long}").raw["address"]
+            if not result["MailingCountry"]:
+                result["MailingCountry"] = addr.get("country_code", "").upper()
+            if not result["MailingState"]:
+                result["MailingState"] = addr.get("state", "")
+            if not result["MailingCity"]:
+                result["MailingCity"] = addr.get("city", "")
+            if not result["MailingPostalCode"]:
+                result["MailingPostalCode"] = addr.get("postcode", "")
+
+        # Finally, see if the city, state/province, country was set. This data comes from us, not
+        # the IP address.
+        elif (
+            city_str_split := mailchimp_data["City, State/Province, Country"].split(",")
+        ) and len(city_str_split) == 3:
+            city, state, country = city_str_split
+            if not result["MailingCountry"]:
+                result["MailingCountry"] = country.strip()
+            if not result["MailingState"]:
+                result["MailingState"] = state.strip()
+            if not result["MailingCity"]:
+                result["MailingCity"] = city.strip()
 
     # Normalize country code.
     if result["MailingCountry"] in ("US", "United States"):
@@ -66,7 +110,7 @@ def process_row(
         and result["MailingPostalCode"]
         and (not result["MailingState"] or not result["MailingCity"])
     ):
-        zipcode_info = SearchEngine().by_zipcode(result["MailingPostalCode"])
+        zipcode_info = zipcode_search_engine.by_zipcode(result["MailingPostalCode"])
         if not result["MailingState"]:
             result["MailingState"] = zipcode_info.state
         if not result["MailingCity"]:
