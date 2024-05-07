7d9309ac8d3c49c47e1864689f1dcb9b915cc7cd
Eric Arellano <ericarellano@me.com>
2024-05-06 23:44:58 -0400

    Make mailchimp processing a method

--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
 import csv
 from pathlib import Path
-from typing import Any
 
 from geopy import Nominatim
 from uszipcode import SearchEngine
 
 from mailchimp_entry import MailchimpEntry
 from salesforce_entry import SalesforceEntry
-from hardcoded_addresses import ADDRESS_STRINGS
 
 
 def main() -> None:
@@ -21,69 +19,19 @@ def main() -> None:
     us_zip_to_metro_name = read_us_zip_to_metro("data/us-zip-to-metro.csv")
     zipcode_search_engine = SearchEngine()
     geocoder = Nominatim(user_agent="parking_reform_network_data_enrichment")
+
     for entry in entries:
-        process_salesforce_entry(
-            entry,
-            zipcode_search_engine,
-            geocoder,
-            mailchimp_by_email,
-            us_zip_to_metro_name,
-        )
+        # The order of operations matters.
+        entry.populate_via_mailchimp(mailchimp_by_email.get(entry.email), geocoder)
+        entry.normalize()
+        entry.populate_city_via_zipcode(zipcode_search_engine)
+        entry.populate_metro_area(us_zip_to_metro_name)
 
     with Path("data/result.csv").open("w", newline="") as f:
         result = [entry.model_dump(by_alias=True) for entry in entries]
         writer = csv.DictWriter(f, fieldnames=result[0].keys(), quoting=csv.QUOTE_ALL)
         writer.writeheader()
         writer.writerows(result)
-
-
-def process_salesforce_entry(
-    entry: SalesforceEntry,
-    zipcode_search_engine: SearchEngine,
-    geocoder: Nominatim,
-    mailchimp_by_email: dict[str, MailchimpEntry],
-    us_zip_to_metro_name: dict[str, str],
-) -> None:
-    # Enrich Salesforce location data with Mailchimp data.
-    if entry.email in mailchimp_by_email and (
-        not entry.country or not entry.state or not entry.city or not entry.zipcode
-    ):
-        mailchimp_entry = mailchimp_by_email[entry.email]
-
-        # If there's an exact address, use that.
-        if mailchimp_entry.address:
-            addr = ADDRESS_STRINGS[mailchimp_entry.address]
-            entry.country = addr.country
-            entry.state = addr.state
-            entry.city = addr.city
-            entry.zipcode = addr.postal
-            entry.street = addr.street
-
-        # Next, reverse geocode the lat/long. This allows us to get a zip code.
-        elif mailchimp_entry.latitude and mailchimp_entry.longitude:
-            entry.latitude = mailchimp_entry.latitude
-            entry.longitude = mailchimp_entry.longitude
-
-            addr = geocoder.reverse(
-                f"{mailchimp_entry.latitude}, {mailchimp_entry.longitude}"
-            ).raw["address"]
-            if not entry.country:
-                entry.country = addr.get("country_code", "").upper()
-            if not entry.state:
-                entry.state = addr.get("state", "")
-            if not entry.city:
-                entry.city = addr.get("city", "")
-            if not entry.zipcode:
-                entry.zipcode = addr.get("postcode", "")
-
-        # Finally, see if the city, state/province, country was set. This data comes from us, not
-        # the IP address.
-        elif mailchimp_entry.city_state_country:
-            entry.city, entry.state, entry.country = mailchimp_entry.city_state_country
-
-    entry.normalize()
-    entry.populate_city_via_zipcode(zipcode_search_engine)
-    entry.populate_metro_area(us_zip_to_metro_name)
 
 
 def read_us_zip_to_metro(fp: str) -> dict[str, str]:
--- src/salesforce_entry.py
+++ src/salesforce_entry.py
@@ -1,7 +1,12 @@
+from typing import Optional
+
+from geopy import Nominatim
 from uszipcode import SearchEngine
 from pydantic import BaseModel, Field
 
+from mailchimp_entry import MailchimpEntry
 from state_codes import US_STATES_TO_CODES
+from hardcoded_addresses import ADDRESS_STRINGS
 
 
 class SalesforceEntry(BaseModel):
@@ -36,6 +41,47 @@ class SalesforceEntry(BaseModel):
         if self.country == "USA" and len(self.zipcode) > 5:
             assert self.zipcode[5] == "-"
             self.zipcode = self.zipcode[:5]
+
+    def populate_via_mailchimp(
+        self, mailchimp: Optional[MailchimpEntry], geocoder: Nominatim
+    ) -> None:
+        if mailchimp is None or (
+            self.country and self.state and self.city and self.zipcode
+        ):
+            return
+
+        # If there's an exact address, use that.
+        if mailchimp.address:
+            addr = ADDRESS_STRINGS[mailchimp.address]
+            self.country = addr.country
+            self.state = addr.state
+            self.city = addr.city
+            self.zipcode = addr.postal
+            self.street = addr.street
+            return
+
+        # Next, reverse geocode the lat/long. This allows us to get a zip code.
+        if mailchimp.latitude and mailchimp.longitude:
+            self.latitude = mailchimp.latitude
+            self.longitude = mailchimp.longitude
+
+            addr = geocoder.reverse(f"{mailchimp.latitude}, {mailchimp.longitude}").raw[
+                "address"
+            ]
+            if not self.country:
+                self.country = addr.get("country_code", "").upper()
+            if not self.state:
+                self.state = addr.get("state", "")
+            if not self.city:
+                self.city = addr.get("city", "")
+            if not self.zipcode:
+                self.zipcode = addr.get("postcode", "")
+            return
+
+        # Finally, see if the city, state/province, country was set. This data comes from us, not
+        # the IP address.
+        if mailchimp.city_state_country:
+            self.city, self.state, self.country = mailchimp.city_state_country
 
     def populate_city_via_zipcode(self, zipcode_search_engine: SearchEngine) -> None:
         """Look up city and state for US zip codes."""
