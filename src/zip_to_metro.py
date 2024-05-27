630940419022f9703130a652ba7b1ace543d4fae
Eric Arellano <ericarellano@me.com>
2024-05-26 20:43:13 -0400

    Ignore data folder

--- src/zip_to_metro.py
+++ src/zip_to_metro.py
@@ -10,7 +10,7 @@ def read_us_zip_to_metro() -> dict[str, str]:
     key = os.environ.pop("ENCRYPTION_KEY")
     cipher = Fernet(key)
 
-    encrypted_data = Path("data/us-zip-to-metro.encrypted.csv").read_bytes()
+    encrypted_data = Path("us-zip-to-metro.encrypted.csv").read_bytes()
     data = cipher.decrypt(encrypted_data).decode("utf-8")
     return {
         row["Zip Code"]: row["Primary CBSA Name"]
