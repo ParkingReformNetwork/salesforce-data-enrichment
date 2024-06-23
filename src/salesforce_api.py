50f6f3ae9fb37284d24a4578db93cab3b6321eda
Eric Arellano <ericarellano@me.com>
2024-06-23 17:19:13 -0400

    Add CI and MyPy

--- src/salesforce_api.py
+++ src/salesforce_api.py
@@ -5,14 +5,17 @@ from simple_salesforce import Salesforce
 from salesforce_entry import SalesforceEntry
 
 
-INSTANCE_URL = os.environ.pop("SALESFORCE_INSTANCE_URL")
-TOKEN = os.environ.pop("SALESFORCE_TOKEN")
-sf = Salesforce(instance_url=INSTANCE_URL, session_id=TOKEN)
+def init_salesforce_client() -> Salesforce:
+    INSTANCE_URL = os.environ.pop("SALESFORCE_INSTANCE_URL")
+    TOKEN = os.environ.pop("SALESFORCE_TOKEN")
+    return Salesforce(instance_url=INSTANCE_URL, session_id=TOKEN)
 
 
-def load_salesforce_data() -> list[SalesforceEntry]:
-    fields = ", ".join(info.alias for info in SalesforceEntry.model_fields.values())
+def load_salesforce_data(client: Salesforce) -> list[SalesforceEntry]:
+    fields = ", ".join(
+        info.alias or name for name, info in SalesforceEntry.model_fields.items()
+    )
     return [
         SalesforceEntry(**raw)
-        for raw in sf.query_all_iter(f"SELECT {fields} FROM Contact")
+        for raw in client.query_all_iter(f"SELECT {fields} FROM Contact")
     ]
